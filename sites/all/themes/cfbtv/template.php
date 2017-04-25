d279d0ac8c690020bbc8d07376edf262b4cf50bf
Micah Mutrux <micah.mutrux@gmail.com>
2017-04-25 08:08:27 -0400

    THEME: create fonts.css

--- sites/all/themes/cfbtv/template.php
+++ sites/all/themes/cfbtv/template.php
@@ -17,4 +17,20 @@ function cfbtv_preprocess_page(&$variables, $hook) {
     if(isset($fid) && file_load($fid) && !variable_get('hero_image_url')) {
         variable_set('hero_image_url', file_create_url(file_load($fid)->uri)); 
     }
+    
+    // suggest themes like page__contentType.tpl.php
+    __theme_page_suggestion($variables);
+}
+
+/**
+ * By default Drupal does not look for a template of type "page__[contentType].tpl.php"
+ * This hook suggests exactly that.  This is what allows me to set a special layout
+ * for the projects page and put some project links/info in the right sidebar.
+ * @param string $variables
+ */
+function __theme_page_suggestion(&$variables) {
+    if (isset($variables['node']->type)) {
+        $nodetype = $variables['node']->type;
+        $variables['theme_hook_suggestions'][] = 'page__' . $nodetype;
+    }
 }
